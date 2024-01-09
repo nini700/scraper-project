3456b23f4b1e62a085de0f17fd2810f6c1562e0a
TheActualSanny <148360536+TheActualSanny@users.noreply.github.com>
2024-01-09 23:08:13 +0400

    Update main-scraper.py

--- main-scraper.py
+++ main-scraper.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Laptop:
-    def __init__(self, curr_price, processor, os, graphics, memory, storage, display):
+    def __init__(self, model, curr_price, processor, os, graphics, memory, storage, display):
+        self.__model = model
         self.__curr_price = curr_price
         self.__processor = processor
         self.__os = os
@@ -14,7 +15,7 @@ class Laptop:
         self.__memory = memory
         self.__storage = display
     def get_Data(self):
-        return [self.__curr_price, self.__processor, self.__os, self.__graphics, self.__memory, self.__storage]
+        return [self.__model, self.__curr_price, self.__processor, self.__os, self.__graphics, self.__memory, self.__storage]
 
 url = 'https://www.dell.com/en-us/shop/dell-laptops/sr/laptops/xps?appliedRefinements=37868,37873,37869,37867,37865'
 headers = {os.getenv('AGENT') : os.getenv('DATA')}
