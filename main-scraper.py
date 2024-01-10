96eb0cd0e3eb4745a0032aa3a14b78ba95be3d17
TheActualSanny <148360536+TheActualSanny@users.noreply.github.com>
2024-01-11 02:32:48 +0400

    Update main-scraper.py
    1. Added a PyDantic class to test out data validation
    2. The program now scrapes every single page

--- main-scraper.py
+++ main-scraper.py
@@ -2,32 +2,57 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from pydantic import BaseModel, field_validator
 
 load_dotenv()
 
-class Laptop:
-    def __init__(self, model, curr_price, processor, os, graphics, memory, storage, display):
-        self.__model = model
-        self.__curr_price = curr_price
-        self.__processor = processor
-        self.__os = os
-        self.__graphics = graphics
-        self.__memory = memory
-        self.__storage = display
-    def get_Data(self):
-        return [self.__model, self.__curr_price, self.__processor, self.__os, self.__graphics, self.__memory, self.__storage]
+class Laptop(BaseModel):
+    model: str
+    curr_price: float
+    processor: str
+    os: str
+    graphics: str
+    memory: str
+    storage: str
+    
+    @field_validator('curr_price', mode = 'before')
+    def price_valid(cls, value):
+        if isinstance(value, str):
+            value.replace(',', '')
+            return float(value)
+        elif isinstance(value, float):
+            return value
+        else:
+            raise TypeError('Type must be a float (or a string)!')
 
+def get_NextPage(url, soup):
+    if params['page'] <= 15:
+        params['page'] += 1
+        return requests.get(url, headers = headers, params = params)
+    else:
+        return 
+
+params = {'page' : 1}
 url = 'https://www.dell.com/en-us/shop/dell-laptops/sr/laptops/xps?appliedRefinements=37868,37873,37869,37867,37865'
 headers = {os.getenv('AGENT') : os.getenv('DATA')}
-req = requests.get(url, headers = headers)
+req = requests.get(url, params = params, headers = headers)
 soup = BeautifulSoup(req.content, 'html.parser')
-first = soup.find_all('article', class_ = 'stack-system ps-stack')
-for laptop in first:
-    data_html = laptop.find_all('span', class_ = 'ps-iconography-specs-label')
-    data = [i.text.lstrip().rstrip() for i in data_html]
-    name = laptop.find('h3', class_ = 'ps-title').text.split('\n')
-    price = laptop.find('div', class_ = 'ps-dell-price ps-simplified').text.split('$')
+laptop_list = []
+
+while True:
+    soup = BeautifulSoup(req.content, 'html.parser')
+    req = get_NextPage(url, soup)
+    if not req:
+        break
+    for laptop in first:
+        data_html = laptop.find_all('span', class_ = 'ps-iconography-specs-label')
+        data = [i.text.lstrip().rstrip() for i in data_html]
+        name = laptop.find('h3', class_ = 'ps-title').text.split('\n')
+        price = laptop.find('div', class_ = 'ps-dell-price ps-simplified').text.split('$')
+        instance = Laptop(model = name[1], curr_price = price, processor = data[0], os = data[1], graphics = data[2], memory = data[3], storage = data[4])
+        laptop_list.append(instance)
 
 
-
-    print(name[1], price[1].rstrip(), data[0], data[1], data[2], data[3], data[4], data[5])
+if __name__ == '__main__':
+    for laptop in laptop_list:
+        print(laptop.curr_price, end = '\n')
